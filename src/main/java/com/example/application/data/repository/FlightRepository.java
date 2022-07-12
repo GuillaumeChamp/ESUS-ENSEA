2144e6647856a2249e944d8e18f9035d6b2ca8f9
Guillaume Champtoussel <80750657+GuillaumeChamp@users.noreply.github.com>
2022-07-12 09:51:25 +0200

    Delete src/main directory

--- src/main/java/com/example/application/data/repository/FlightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.application.data.repository;
-
-import com.example.application.data.entity.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight,Long> {
-}
